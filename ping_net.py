a117c2c404e1796d7a213c1119fd0b48dfe3cd64
xodop <roachggwp@gmail.com>
2022-03-12 12:41:10 +0000

    add dev branch, add all networks

--- ping_net.py
+++ ping_net.py
@@ -16,11 +16,24 @@ def ping_net():
         )
 
     ip_route = ip_route.stdout.split('\n')
-    
+   ''' 
     for line in ip_route:
         if 'br1' in line:                                              #фильтр по подсетке
             subnet = ipaddress.ip_network(line.split()[0])
-            
+      '''
+    net = [] 
+    i = 0 
+    for line in ip_route: 
+        try: 
+            net = ipaddress.ip_network(ip_route[i].split()[0]) 
+            nets.append(net) 
+        except ValueError: 
+            pass 
+        except IndexError: 
+            break 
+        finally: 
+            i += 1 
+                
     pp_ip = []
     pp_processes = []
     for ip in subnet.hosts():
@@ -39,6 +52,7 @@ def ping_net():
             reachable.append(str(ip))
         else:
             unreachable.append(str(ip))
+    
             
     return reachable, unreachable
     
@@ -46,8 +60,7 @@ def ping_net():
 
 if __name__ == "__main__":
     reachable, unreachable = ping_net()
-    print('Доступные адреса | Недоступные адреса')
-    print(f'{"":17}{"|"}')
+    print(f'Доступные адреса | Недоступные адреса\n{"="*40}')
     for r, u in itertools.zip_longest(reachable, unreachable, fillvalue=' '): 
         print(f'{r:17}{"|":2}{u}') 
             
