e1275bcee52c9fe892d68a943ebe6a925aeb7399
xodop <roachggwp@gmail.com>
2022-03-14 15:10:49 +0000

    add parsing interfaces match descriptions

--- descriptinator.py
+++ descriptinator.py
@@ -7,18 +7,24 @@ from pprint import pprint
 with open(r'MBH_25_00015_1-220223_1508.log') as in_f:
     descr = in_f.read()
 
-#get all descriptors that contain ##
-descr_list = re.findall(r'(?:up|\*?down) +(?:up|down) +([\S ]*#.+#[\S ]*)', descr)
 
+#cоздаем списки интерфейсов и дескрипторов
+iface_and_descr = re.findall(r'(\S+) +(?:up|\*?down) +(?:up|down) +(.*)', descr)
+ifaces = [] 
+descrs = []
+for item in iface_and_descr: 
+    ifaces.append(item[0])
+    descrs.append(item[1])
 
-#make ## the first in descriptor
-for item in descr_list: 
-    i = descr_list.index(item) 
-    if not item.startswith('#'): 
-        item = item.split()
-        item = ' '.join(sorted(item))
-        descr_list.pop(i) 
-        descr_list.insert(i, item) 
+#перемещаем #текст# при наличии в начало дескриптора
+for item in descrs:
+    if re.findall(r'#.*#', item): 
+        i = descrs.index(item) 
+        if not item.startswith('#'): 
+            item = item.split()
+            item = ' '.join(sorted(item))
+            descrs.pop(i) 
+            descrs.insert(i, item)
 
     
-pprint(descr_list)
+pprint(descrs)
