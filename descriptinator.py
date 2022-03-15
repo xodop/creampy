fba3be63d6d2c5684fc288c7c8c714cc8f88042c
xodop <roachggwp@gmail.com>
2022-03-15 03:47:28 +0000

    add export result to file and delet redundant = and - from descriptions

--- descriptinator.py
+++ descriptinator.py
@@ -4,8 +4,8 @@ import re
 from pprint import pprint
 
 
-with open(r'MBH_25_00015_1-220223_1508.log') as in_f:
-    descr = in_f.read()
+with open(r'MBH_25_00015_1-220223_1508.log') as f:
+    descr = f.read()
 
 
 #cоздаем списки интерфейсов и дескрипторов
@@ -16,15 +16,28 @@ for item in iface_and_descr:
     ifaces.append(item[0])
     descrs.append(item[1])
 
-#перемещаем #текст# при наличии в начало дескриптора
+
 for item in descrs:
+    i = descrs.index(item)
+#перемещаем #текст# при наличии в начало дескриптора
     if re.findall(r'#.*#', item): 
-        i = descrs.index(item) 
         if not item.startswith('#'): 
             item = item.split()
             item = ' '.join(sorted(item))
             descrs.pop(i) 
             descrs.insert(i, item)
+#заменяет === на ---
+    elif re.search(r'={2,}', item):
+        item = item.replace('=', '-')
+        descrs.pop(i)
+        descrs.insert(i, item)
+#
+    elif re.search(r'-{2,}', item):
+        item = item.replace('-', '')
+        descrs.pop(i)
+        descrs.insert(i, item)
+        
 
-    
-pprint(descrs)
+descrs = '\n'.join(descrs)
+with open(r'output.log', 'w') as f:
+    descr = f.write(descrs)
